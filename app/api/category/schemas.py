29b6854bc202188a5c3482703520159b97927f28
osha890 <oshavshav@gmail.com>
2025-09-20 20:01:15 +0300

    create category schemas

---
+++ app/api/category/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class CategoryBase(BaseModel):
+    name: str
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class CategoryRead(CategoryBase):
+    id: int
+
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
+
+class CategoryPartial(BaseModel):
+    name: str | None = None
